1a9a0c55e522c6f03a4bcaf5c84dc9f3a5e22316
ismadevjs <agencedz213@gmail.com>
2024-04-24 18:12:21 +0100

    a user is connected now

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const { createServer } = require('node:http')
-const { join } = require('node:path');
+const express = require("express");
+const { createServer } = require("http"); 
+const { join } = require("path");
+const { Server } = require("socket.io");
 
-const app = express()
-const server = createServer(app)
+const app = express();
+const server = createServer(app); 
+const io = new Server(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
 });
 
-app.listen('3000', () => console.log('connected : 3000'))
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log("connected : 3000");
+});

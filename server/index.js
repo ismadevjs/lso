a52985f601fd19d31c34476cc034dfdb6f8817d1
ismadevjs <agencedz213@gmail.com>
2024-04-24 18:34:39 +0100

    the mesasge now can be logged in the console

--- server/index.js
+++ server/index.js
@@ -1,19 +1,28 @@
 const express = require("express");
-const { createServer } = require("http"); 
+const { createServer } = require("http");
 const { join } = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
-const server = createServer(app); 
+const server = createServer(app);
 const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+
+io.on("connection", (socket) => {
+    console.log('logged')
+    socket.on("chat message", (msg) => {
+      console.log("message: " + msg);
+    });
+
+    socket.on('disconnect', () => console.log('disconnected'))
 });
+
+
+
 
 server.listen(3000, () => {
   console.log("connected : 3000");

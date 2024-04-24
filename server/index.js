d1aefd8ad706a42fe50b3a58d1e24edfb4a365d0
ismadevjs <agencedz213@gmail.com>
2024-04-24 18:52:19 +0100

    io sends to be to all sockets.

--- server/index.js
+++ server/index.js
@@ -14,15 +14,19 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
     console.log('logged')
+
     socket.on("chat message", (msg) => {
       console.log("message: " + msg);
+      io.emit('chat message', msg)
     });
 
     socket.on('disconnect', () => console.log('disconnected'))
 });
 
 
-
+// io.on('connection', (socket) => {
+//     socket.broadcast.emit('hi')
+// })
 
 server.listen(3000, () => {
   console.log("connected : 3000");
